116677f0443f9342277d876f635c9daba74987eb
anfiquehussain <adomax2003@gmail.com>
2024-10-24 12:27:23 +0530

    api changed

--- src/constant/api.jsx
+++ src/constant/api.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://195.35.20.1:8000/accounts/";
+export const BASE_URL = "https://ad4d-111-92-96-106.ngrok-free.app/accounts/";
+// https://ad4d-111-92-96-106.ngrok-free.app
