4cbe98228bfa8bae730e467b9d2a0338c38a96b9
anfiquehussain <adomax2003@gmail.com>
2024-10-25 16:29:15 +0530

    service page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card({ img, title, subtitle }) {
     return (
-        <div className="w-3/4 md:w-1/3 lg:w-1/4 mx-auto rounded-2xl overflow-hidden border-green-500 border-2 px-4 py-10 bg-white shadow-md">
+        <div className="w-full md:w-1/3 lg:w-1/4 mx-auto rounded-2xl overflow-hidden border-green-500 border-2 px-4 py-10 bg-white shadow-md">
             <img className="w-full h-max object-cover" src={img} alt={title} />
             <div className="px-4 py-4 text-start ">
                 <h3 className="font-bold text-lg md:text-xl mb-2 text-gray-900">{title}</h3>
